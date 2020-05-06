a00b210e7671fc54fee00b4dd4e43da9c2abab16
Kolovos Konstantinos <barbasgr@gmail.com>
2020-05-06 20:52:57 +0300

    add icons

--- src/app/storage/storage-list/storage-item/storage-item.component.ts
+++ src/app/storage/storage-list/storage-item/storage-item.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Storage} from '../../../apiEntities/storage-entity.model';
+import {faEuroSign, faBoxes} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
     selector: 'app-storage-item',
@@ -10,6 +11,9 @@ export class StorageItemComponent implements OnInit {
 
     @Input() storage: Storage;
     @Input() index: number;
+
+    faEuroSign = faEuroSign;
+    faBoxes = faBoxes;
 
     constructor() {
     }
