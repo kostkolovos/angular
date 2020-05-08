cce5ed24b1c8d5aaec4fa4e8871772640463afd4
Kolovos Konstantinos <barbasgr@gmail.com>
2020-05-08 11:10:02 +0300

    fix resolver for childs

--- src/app/storage/storage-resolver.service.ts
+++ src/app/storage/storage-resolver.service.ts
@@ -3,16 +3,18 @@ import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/rou
 import {Storage} from '../apiEntities/storage-entity.model';
 import {Observable} from 'rxjs';
 import {StorageService} from './storage.service';
+import {StorageTypeService} from './storage-type.service';
 
 @Injectable({
     providedIn: 'root'
 })
 export class StorageResolverService implements Resolve<Storage[]> {
 
-    constructor(private storageService: StorageService) {
+    constructor(private storageService: StorageService, private storageTypeService: StorageTypeService) {
     }
 
     resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Storage[]> | Promise<Storage[]> | Storage[] {
+        this.storageTypeService.getstorageTypes();
         const storages = this.storageService.getStorages();
         if (storages.length === 0) {
             return this.storageService.fetchStorages();
