198ba0b4d41a5f82f42d966702f5c92b75ba4e3c
Kolovos Konstantinos <barbasgr@gmail.com>
2020-05-08 19:49:19 +0300

    better required

--- src/app/storage/storage-edit/storage-edit.component.ts
+++ src/app/storage/storage-edit/storage-edit.component.ts
@@ -82,8 +82,9 @@ export class StorageEditComponent implements OnInit {
             storagePetTypes: storagePetType
         });
 
-        const storagePetTypesControl = this.storageForm.get('storagePetTypes');
-        const microchipStoragePetTypesControl = storagePetTypesControl['controls'][0].get('microchip');
+        const arrayControl = this.storageForm.get('storagePetTypes') as FormArray;
+        const microchipStoragePetTypesControl = arrayControl.controls.find(Boolean).get('microchip');
+
         this.storageForm.get('storageTypes').valueChanges.subscribe((val) => {
             this.currentType = val;
             if (this.currentType.title === this.storagePetTypeValue) {
