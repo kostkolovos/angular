1c8ea634593a8a84c09a072df72c244157de9dad
Kolovos Konstantinos <barbasgr@gmail.com>
2020-05-10 11:10:26 +0300

    add hidden in small devices

--- src/app/storage/storage.component.ts
+++ src/app/storage/storage.component.ts
@@ -6,11 +6,46 @@ import {Component, OnInit} from '@angular/core';
     styleUrls: ['./storage.component.css']
 })
 export class StorageComponent implements OnInit {
+    hasChanged = false;
+    hideList: boolean;
+    hideItems: boolean;
 
     constructor() {
     }
 
     ngOnInit(): void {
+        if (window.screen.width < 768 && !this.hasChanged) { // 768px portrait
+            this.hideItems = true;
+            this.hideList = false;
+        } else if (window.screen.width < 768 && !this.hasChanged) {
+            this.hideItems = false;
+            this.hideList = false;
+        }
+    }
+
+
+    onActivate(event: any) {
+        const constructorName = event.constructor.name;
+
+        if (window.screen.width < 768 && constructorName === 'StorageStartComponent') {
+            this.hideItems = true;
+            this.hideList = false;
+        } else if (window.screen.width < 768 && constructorName !== 'StorageStartComponent') {
+            this.hideItems = false;
+            this.hideList = true;
+        }
+
+        this.hasChanged = true;
+    }
+
+    onDeactivate(event: any) {
+        const constructorName = event.constructor.name;
+        if (window.screen.width < 768 && constructorName === 'StorageStartComponent') {
+            this.hideItems = true;
+            this.hideList = false;
+        }
+
+        this.hasChanged = true;
     }
 
 }
