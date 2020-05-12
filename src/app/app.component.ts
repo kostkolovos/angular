5ca7eafe2da7cb9f078d05fc95342930e9ff1390
Kolovos Konstantinos <barbasgr@gmail.com>
2020-05-12 17:20:28 +0300

    Add title index

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,20 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from './auth/auth.service';
+import {Title} from '@angular/platform-browser';
+import {environment} from '../environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
 
-  constructor(private authService: AuthService) {
-  }
+    constructor(private authService: AuthService, private titleService: Title) {
+    }
 
-  ngOnInit(): void {
-    this.authService.autoLogin();
-  }
+    ngOnInit(): void {
+        this.authService.autoLogin();
+        this.titleService.setTitle(environment.applicationTitle);
+    }
 }
