02d639b6027113a9d1db6f615be25150e82cc349
Kolovos Konstantinos <barbasgr@gmail.com>
2020-05-15 22:04:20 +0300

    add pieces and genders

--- src/app/order/order-list/order-item/order-item.component.ts
+++ src/app/order/order-list/order-item/order-item.component.ts
@@ -1,5 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Order} from '../../../apiEntities/order-entity.model';
+import {faBoxes, faMars, faVenus} from '@fortawesome/free-solid-svg-icons';
+import {StorageTypeService} from '../../../storage/storage-type.service';
 
 @Component({
     selector: 'app-order-item',
@@ -7,11 +9,15 @@ import {Order} from '../../../apiEntities/order-entity.model';
     styleUrls: ['./order-item.component.css']
 })
 export class OrderItemComponent implements OnInit {
+    faBoxes = faBoxes;
+    faMars = faMars;
+    faVenus = faVenus;
+    storagePetTypeValue = this.storageTypeService.getStoragePetTypeValue();
 
     @Input() order: Order;
     @Input() index: number;
 
-    constructor() {
+    constructor(private storageTypeService: StorageTypeService) {
     }
 
     ngOnInit(): void {
