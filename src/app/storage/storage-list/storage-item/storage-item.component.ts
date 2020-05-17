48e3c63fdbff0d581d554ca77033cd9f5c6b2689
Kolovos Konstantinos <barbasgr@gmail.com>
2020-05-17 11:18:32 +0300

    change icons

--- src/app/storage/storage-list/storage-item/storage-item.component.ts
+++ src/app/storage/storage-list/storage-item/storage-item.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from '@angular/core';
 import {Storage} from '../../../apiEntities/storage-entity.model';
-import {faEuroSign, faBoxes, faEllipsisV} from '@fortawesome/free-solid-svg-icons';
+import {faEuroSign, faBoxes, faEllipsisV, faPaw, faMars, faVenus} from '@fortawesome/free-solid-svg-icons';
+import {StorageTypeService} from '../../storage-type.service';
 
 @Component({
     selector: 'app-storage-item',
@@ -8,15 +9,19 @@ import {faEuroSign, faBoxes, faEllipsisV} from '@fortawesome/free-solid-svg-icon
     styleUrls: ['./storage-item.component.css']
 })
 export class StorageItemComponent implements OnInit {
-
     @Input() storage: Storage;
     @Input() index: number;
 
+    storagePetTypeValue = this.storageTypeService.getStoragePetTypeValue();
     faEuroSign = faEuroSign;
     faBoxes = faBoxes;
     faEllipsisV = faEllipsisV;
+    faPaw = faPaw;
+    faMars = faMars;
+    faVenus = faVenus;
 
-    constructor() {
+
+    constructor(private storageTypeService: StorageTypeService) {
     }
 
     ngOnInit(): void {
